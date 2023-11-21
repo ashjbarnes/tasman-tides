1da0c459858ad2f5642729af1f008c531f3aa95e
ashjbarnes <ashjbarnes97@gmail.com>
2023-11-21 16:03:46 +1100

    full-40 expt

--- rundirs/full-40/postprocessing.sh
+++ rundirs/full-40/postprocessing.sh
@@ -5,10 +5,11 @@
 #PBS -l mem=112gb
 #PBS -l walltime=6:00:00
 #PBS -l ncpus=28
+#PBS -l jobfs=100gb
 #PBS -l storage=gdata/v45+scratch/v45+scratch/x77+gdata/v45+gdata/nm03+gdata/hh5
 cd $PBS_O_WORKDIR
 PYTHONNOUSERSITE=1
 module use /g/data/hh5/public/modules
 module load conda/analysis3-unstable
 module list
-python3 /home/149/ab8992/tasman-tides/postprocessing.py -p 0
+python3 /home/149/ab8992/tasman-tides/postprocessing.py -p -1 -c 6
