c22c363923193eb9dc3935613554d66539b4438b
ashjbarnes <ashjbarnes97@gmail.com>
2023-11-21 18:10:24 +1100

    2023-11-21 18:10:24: Run 0

--- rundirs/full-20/postprocessing.sh
+++ rundirs/full-20/postprocessing.sh
@@ -2,14 +2,14 @@
 #PBS -N ttide-pprocess
 #PBS -P v45
 #PBS -q normalbw
-#PBS -l mem=112gb
+#PBS -l mem=224gb
 #PBS -l walltime=6:00:00
-#PBS -l ncpus=28
-#PBS -l jobfs=100gb
+#PBS -l ncpus=56
+#PBS -l jobfs=200gb
 #PBS -l storage=gdata/v45+scratch/v45+scratch/x77+gdata/v45+gdata/nm03+gdata/hh5
 cd $PBS_O_WORKDIR
 PYTHONNOUSERSITE=1
 module use /g/data/hh5/public/modules
 module load conda/analysis3-unstable
 module list
-python3 /home/149/ab8992/tasman-tides/postprocessing.py -p 0
+python3 /home/149/ab8992/tasman-tides/postprocessing.py -p "last"
