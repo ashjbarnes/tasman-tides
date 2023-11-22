f745908c02f09e0a83932e94582b012d6250e501
ashjbarnes <ashjbarnes97@gmail.com>
2023-11-22 17:06:20 +1100

    syncing local to gadi

---
+++ notebooks/analysis/hef_movie.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#PBS -N ttide-pprocess
+#PBS -P v45
+#PBS -q normalbw
+#PBS -l mem=112gb
+#PBS -l walltime=6:00:00
+#PBS -l ncpus=28
+#PBS -l jobfs=2gb
+#PBS -l storage=gdata/v45+scratch/v45+scratch/x77+gdata/v45+gdata/nm03+gdata/hh5
+cd $PBS_O_WORKDIR
+PYTHONNOUSERSITE=1
+module use /g/data/hh5/public/modules
+module load conda/analysis3-unstable
+module list
+python3 /home/149/ab8992/tasman-tides/notebooks/analysis/hef_movie.py
