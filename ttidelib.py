e21847fcb1275f21c9e46242c9c3bbbc9b2e20ef
ashjbarnes <ashjbarnes97@gmail.com>
2023-12-01 12:07:07 +1100

    2023-12-01 12:07:07: Run 18

--- ttidelib.py
+++ ttidelib.py
@@ -215,11 +215,11 @@ def collect_data(exptname,rawdata = None,ppdata = None,surface_data = None,bathy
     data = {}
     if type(rawdata) != type(None):
         for var in rawdata:
-            data[var] = xr.open_mfdataset(str(rawdata_path / var / "*"),chunks = chunks,decode_times = False).isel(time = slice(timerange[0],timerange[1]))
+            data[var] = xr.open_mfdataset(str(rawdata_path / var / "*"),chunks = chunks,decode_times = False)[var].isel(time = slice(timerange[0],timerange[1]))
 
     if type(ppdata) != type(None):
         for var in ppdata:
-            data[var] = xr.open_mfdataset(str(ppdata_path / var / "*"),chunks = chunks,decode_times = False).isel(TIME = slice(timerange[0],timerange[1]))
+            data[var] = xr.open_mfdataset(str(ppdata_path / var / "*"),chunks = chunks,decode_times = False)[var].isel(TIME = slice(timerange[0],timerange[1]))
 
     if type(surface_data) != type(None):
         for var in surface_data:
