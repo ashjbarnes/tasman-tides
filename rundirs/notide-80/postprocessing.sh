95571c2e75f46338f8743b2d269b6de9b87faf24
ashjbarnes <ashjbarnes97@gmail.com>
2023-12-09 19:40:23 +1100

    2023-12-09 19:40:23: Run 15

--- rundirs/notide-80/postprocessing.sh
+++ rundirs/notide-80/postprocessing.sh
@@ -5,7 +5,7 @@
 #PBS -l mem=112gb
 #PBS -l walltime=6:00:00
 #PBS -l ncpus=28
-#PBS -l storage=gdata/v45+scratch/v45+scratch/x77+gdata/v45+gdata/nm03+gdata/hh5
+#PBS -l storage=gdata/v45+scratch/v45+scratch/x77+gdata/v45+gdata/nm03+gdata/hh5+scratch/nm03
 cd $PBS_O_WORKDIR
 PYTHONNOUSERSITE=1
 module use /g/data/hh5/public/modules
