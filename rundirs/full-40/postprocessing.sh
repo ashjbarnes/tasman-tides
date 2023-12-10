57181121d82a2349e98baadcec89d1807d49fb6e
ashjbarnes <ashjbarnes97@gmail.com>
2023-12-11 10:01:19 +1100

    2023-12-11 10:01:19: Run 1

--- rundirs/full-40/postprocessing.sh
+++ rundirs/full-40/postprocessing.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-#PBS -N ttide-pprocess
+#PBS -N full40-pprocess
 #PBS -P v45
 #PBS -q normalbw
 #PBS -l mem=112gb
 #PBS -l walltime=6:00:00
 #PBS -l ncpus=28
 #PBS -l storage=gdata/v45+scratch/v45+scratch/x77+gdata/v45+gdata/nm03+gdata/hh5+scratch/nm03
+#PBS -l jobfs=10gb
 cd $PBS_O_WORKDIR
 PYTHONNOUSERSITE=1
 module use /g/data/hh5/public/modules
--- rundirs/notide-80/postprocessing.sh
+++ rundirs/notide-80/postprocessing.sh
@@ -6,6 +6,7 @@
 #PBS -l walltime=6:00:00
 #PBS -l ncpus=28
 #PBS -l storage=gdata/v45+scratch/v45+scratch/x77+gdata/v45+gdata/nm03+gdata/hh5+scratch/nm03
+#PBS -l jobfs=10gb
 cd $PBS_O_WORKDIR
 PYTHONNOUSERSITE=1
 module use /g/data/hh5/public/modules
