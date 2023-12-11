dadf5f5b39429fb7832d8533f3b8b04cb0d06a0e
ashjbarnes <ashjbarnes97@gmail.com>
2023-12-11 15:27:34 +1100

    2023-12-11 15:27:34: Run 21

--- recipes.py
+++ recipes.py
@@ -68,7 +68,9 @@ def save_ppdata(transect_data,topdown_data,basepath,recompute = False):
     Save the postprocessed data to gdata. Takes computed topdown and transect data and saves each time slice to postprocessed folders
     Time index override is used when processing one time slice at a time. That way the index can be used to name the file correctly
     """
-
+    print(basepath)
+    print(basepath.name)
+    print(str(type(basepath.name)))
     for i in ["topdown","transect"]:
         if not os.path.exists(basepath / i):
             os.makedirs(basepath / i)
@@ -76,10 +78,10 @@ def save_ppdata(transect_data,topdown_data,basepath,recompute = False):
     for i in range(len(topdown_data.time.values)):
         time = topdown_data.time.values[i]
         if not os.path.exists(basepath / "topdown" / f"vorticity_time-{str(i).zfill(3)}.nc") or recompute:
-            topdown_data.isel(time = i).expand_dims("time").assign_coords(time = [time]).to_netcdf(basepath / "topdown" / basepath.name + f"_time-{str(round(time))}.nc")
+            topdown_data.isel(time = i).expand_dims("time").assign_coords(time = [time]).to_netcdf(basepath / "topdown" / str(basepath.name + f"_time-{str(round(time))}.nc"))
 
         if not os.path.exists(basepath / "transect" / f"vorticity_time-{str(i).zfill(3)}.nc") or recompute:
-            transect_data.isel(time = i).expand_dims("time").assign_coords(time = [time]).to_netcdf(basepath / "transect" / basepath.name + f"_time-{str(round(time))}.nc")
+            transect_data.isel(time = i).expand_dims("time").assign_coords(time = [time]).to_netcdf(basepath / "transect" / str(basepath.name + f"_time-{str(round(time))}.nc"))
 
 
     return
