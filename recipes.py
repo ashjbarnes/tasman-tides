c292b13a217fcaa9ec9ad3cdd319edefd2be8fbc
ashjbarnes <ashjbarnes97@gmail.com>
2024-01-16 10:17:03 +1100

    2024-01-16 10:17:03: Run 13

--- recipes.py
+++ recipes.py
@@ -244,7 +244,7 @@ def qsub(recipe, experiment, outputs):
 #PBS -l mem=112gb
 #PBS -l walltime=6:00:00
 #PBS -l ncpus=28
-#PBS -l jobfs=2gb
+#PBS -l jobfs=100gb
 #PBS -l storage=gdata/v45+scratch/v45+scratch/x77+gdata/v45+gdata/nm03+gdata/hh5
 PYTHONNOUSERSITE=1
 module use /g/data/hh5/public/modules
