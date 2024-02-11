a6d25f4249123286637efed6e9f01f0017ccd5ea
ashjbarnes <ashjbarnes97@gmail.com>
2024-02-11 19:30:51 +1100

    add nm03 scratch to qsub

--- recipes.py
+++ recipes.py
@@ -302,7 +302,7 @@ def qsub(recipe, experiment, outputs,recompute,chunks):
 #PBS -l walltime=6:00:00
 #PBS -l ncpus=28
 #PBS -l jobfs=100gb
-#PBS -l storage=gdata/v45+scratch/v45+scratch/x77+gdata/v45+gdata/nm03+gdata/hh5
+#PBS -l storage=gdata/v45+scratch/v45+scratch/x77+gdata/v45+gdata/nm03+gdata/hh5+scratch/nm03
 PYTHONNOUSERSITE=1
 module use /g/data/hh5/public/modules
 module load conda/analysis3-unstable
