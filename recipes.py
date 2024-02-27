9aa2d73d4d87f796f377852470761e97ac736476
ashjbarnes <ashjbarnes97@gmail.com>
2024-02-27 14:56:44 +1100

    longer walltime

--- recipes.py
+++ recipes.py
@@ -311,10 +311,10 @@ def qsub(recipe, experiment, outputs,recompute):
     text = f"""
 #!/bin/bash
 #PBS -N {recipe}-{experiment}
-#PBS -P v45
+#PBS -P x77
 #PBS -q normalbw
 #PBS -l mem=112gb
-#PBS -l walltime=6:00:00
+#PBS -l walltime=12:00:00
 #PBS -l ncpus=28
 #PBS -l jobfs=100gb
 #PBS -l storage=gdata/v45+scratch/v45+scratch/x77+gdata/v45+gdata/nm03+gdata/hh5+scratch/nm03
