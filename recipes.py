ee09e82a48e1dd0288336c0f25fa02142806bc33
ashjbarnes <ashjbarnes97@gmail.com>
2024-03-09 08:38:03 +1100

    print statements

--- recipes.py
+++ recipes.py
@@ -299,6 +299,8 @@ def spinup_timeseries(experiment):
 #### LAGRANGE FILTERING
     
 def lagrange_filter(expt,zl,t0,time_window = 200,filter_window = 100,filter_cutoff = 2*np.pi/(12.42*3600)):
+    print("START LAGRANGE FILTERING")
+    print("import filtering package:")
     import filtering
 
     """
