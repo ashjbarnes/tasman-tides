a9aa86f3c174c576e0953f532b9723ee1f96358c
ashjbarnes <ashjbarnes97@gmail.com>
2024-04-30 12:52:43 +0200

    reduce walltime for lfilter

--- recipes.py
+++ recipes.py
@@ -471,7 +471,7 @@ def qsub_lagrange_filter(experiment,zl,t0,windowsize):
 #PBS -P x77
 #PBS -q normalbw
 #PBS -l mem=112gb
-#PBS -l walltime=6:00:00
+#PBS -l walltime=4:00:00
 #PBS -l ncpus=28
 #PBS -l jobfs=100gb
 #PBS -l storage=gdata/v45+scratch/v45+scratch/x77+gdata/v45+gdata/nm03+gdata/hh5+scratch/nm03
