f72ac99ba6968267f9d1e2d93c58a1ddbdd611c5
ashjbarnes <ashjbarnes97@gmail.com>
2024-06-26 16:13:51 +1000

    rename output path for bandpass

--- lfilter.sh
+++ lfilter.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Parse command-line arguments
-while getopts t:e:z: flag
+while getopts t:e:z:w: flag
 do
     case "${flag}" in
         t) tvalue=${OPTARG};;
@@ -22,12 +22,13 @@ then
     # Iterate over the range and run the command
     for i in $(seq ${RANGE[0]} ${RANGE[1]})
     do
-        filepath="/g/data/nm03/ab8992/postprocessed/${evalue}/lfiltered/t0-${tvalue}/highpass_${i}.nc"
-        filepath2="/g/data/nm03/ab8992/postprocessed/${evalue}/lfiltered/t0-${tvalue}/lowpass_${i}.nc"
+        filepath="/g/data/nm03/ab8992/postprocessed/${evalue}/lfiltered/bp-t0-${tvalue}/highpass_${i}.nc"
+        filepath2="/g/data/nm03/ab8992/postprocessed/${evalue}/lfiltered/bp-t0-${tvalue}/lowpass_${i}.nc"
         if [ -f "$filepath" ] && [ -f "$filepath2" ]
         then
             echo "File $filepath exists."
         else
+	    echo "filtering $filepath" 
             python3 recipes.py -r lagrange_filter -e $evalue -t $tvalue -z $i -w $wvalue&
         fi
     done
