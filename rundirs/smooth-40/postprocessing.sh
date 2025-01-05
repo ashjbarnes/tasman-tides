6c04195486572e7939d5b91da54d05bc6127bc15
ashjbarnes <ashjbarnes97@gmail.com>
2025-01-05 11:12:10 +1100

    2025-01-05 11:12:10: Run 31

--- rundirs/smooth-40/postprocessing.sh
+++ rundirs/smooth-40/postprocessing.sh
@@ -8,7 +8,6 @@
 #PBS -l storage=gdata/v45+scratch/v45+scratch/x77+gdata/v45+gdata/nm03+gdata/hh5+scratch/nm03
 cd $PBS_O_WORKDIR
 PYTHONNOUSERSITE=1
-module use /g/data/hh5/public/modules
-module load conda/analysis3-unstable
-module list
+source /g/data/hh5/public/apps/miniconda3/envs/analysis3-24.04/bin/activate
+
 python3 /home/149/ab8992/tasman-tides/postprocessing.py -p "last" -c 6
